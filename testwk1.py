7b5ee4d441994c6cff5477091c802faabf465bcb
adelekevictor12 <124449439+adelekevictor12@users.noreply.github.com>
2023-11-01 22:34:48 +0100

    python assignment added

---
+++ testwk1.py
@@ -0,0 +1,4 @@
+name = input("Please enter your name ")
+age = int(input("Please enter your age "))
+location = input("Please enter your current location ")
+print("user's name "+name,"Your age is "+str(age), "and currently at "+location,end='.')
